e52f819a14aede66ee716febdcd7bd267cf3c5f4
Lucas Alves <113432736+lOliveira2105@users.noreply.github.com>
2025-03-05 11:53:11 +0000

    #1- chore: add .gitignore and initialize MAUI project

---
+++ FitBitesApp/FitBitesApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FitBitesApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
